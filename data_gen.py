74ed0ba3fccb5c1b6c82d32ee708e776319efefa
Leo Bitz <amanyamy@gmail.com>
2019-04-08 17:23:54 -0700

    base

---
+++ data_gen.py
@@ -0,0 +1,103 @@
+import numpy as np
+from text_processor import *
+
+class DataGen:
+
+    def __init__(self, reverse=False, data):
+        if reverse:
+            self.roots, self.words, self.featArray, mr, mw, n_features = get_reverse_feature_array(data)
+            self.n_features = n_features
+            self.pred_feat_shapes = []
+            for feat in self.featArray[0]:
+                self.pred_feat_shapes.append(len(feat))
+        else:
+            self.roots, self.words, self.featArray, mr, mw = get_feature_array(data)
+            self.word_feat_len = len(self.featArray[0])
+        self.n_chars = len(char2int)
+        self.max_root_len = mr
+        self.max_output_len = mw + 2
+        
+
+    def cnn_gen_data(self, batch_size=100, n_batches=-1, trainset=True):
+        max_batch, min_batch = 0, 0
+        if trainset == True:
+            max_batch = int(len(self.words) * .7) / batch_size
+            min_batch = 0
+        else:
+            max_batch = len(self.words)/ batch_size
+            min_batch = int(len(self.words) * .7 / batch_size)
+        
+        total_batchs = max_batch
+        batch = min_batch
+        while True:
+            rootX, target_inX, featX, y = list(), list(), list(), list()
+            for i in range(batch * batch_size, (1 + batch) * batch_size):
+                root = self.roots[i]
+                word = self.words[i]
+                word_feature = self.featArray[i]
+                root_encoded, target_encoded, target_in_encoded = self.encond_input_output(root, word)
+                rootX.append(root_encoded.reshape((root_encoded.shape[0], root_encoded.shape[1], 1)))
+                target_inX.append(target_in_encoded)
+                featX.append(word_feature)
+                y.append(target_encoded)
+            yield [np.array(rootX), np.array(target_inX), np.array(featX)], np.array(y)
+            batch += 1
+            if batch == total_batchs or batch == n_batches:
+                batch = min_batch
+                
+    def rnn_gen_data(self, batch_size=100, n_batches=-1, trainset=True):
+        max_batch, min_batch = 0, 0
+        if trainset == True:
+            max_batch = int(len(self.words) * .7) / batch_size
+            min_batch = 0
+        else:
+            max_batch = len(self.words)/ batch_size
+            min_batch = int(len(self.words) * .7 / batch_size)
+        
+        total_batchs = max_batch
+        batch = min_batch
+        while True:
+            rootX, target_inX, featX, y = list(), list(), list(), list()
+            for i in range(batch * batch_size, (1 + batch) * batch_size):
+                root = self.roots[i]
+                word = self.words[i]
+                word_feature = self.featArray[i]
+                root_encoded, target_encoded, target_in_encoded = self.encond_input_output(root, word)
+                rootX.append(root_encoded.reshape((root_encoded.shape[0], root_encoded.shape[1])))
+                target_inX.append(target_in_encoded)
+                featX.append(word_feature)
+                y.append(target_encoded)
+            yield [np.array(rootX), np.array(target_inX), np.array(featX)], np.array(y)
+            batch += 1
+            if batch == total_batchs or batch == n_batches:
+                batch = min_batch
+                
+
+    def word2vec(self, word, max_chars):
+        vec = np.zeros((max_chars, self.n_chars))
+        for i in range(len(word)):
+            vec[i][char2int[word[i]]] = 1
+        index_sps = char2int[' ']
+        vec[len(word):, index_sps] = 1
+        return vec
+
+    
+    def encond_input_output(self, root_word, target_word):
+        root_word = list(root_word)
+        target_word = list(target_word) + ['&']
+        target_word_in = ["&"] + target_word#[:-1]
+        root_encoded = self.word2vec(root_word, self.max_root_len)
+        target_encoded = self.word2vec(target_word, self.max_output_len)
+        target_in_encoded = self.word2vec(target_word_in, self.max_output_len)
+        return root_encoded, target_encoded, target_in_encoded
+
+    def one_hot_decode(self, vec):
+        return [int2char[np.argmax(v)] for v in vec]
+    
+    def word_sim(self, word1, word2):
+        c = 0
+        for i in range(len(word1)):
+            if word1[i] == word2[i]:
+                c += 1
+        return c/len(word1)
+            
---
+++ models.py
@@ -0,0 +1,67 @@
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import LSTM, TimeDistributed
+from keras.layers import Concatenate, Flatten
+from keras.layers import GRU, Conv2D, MaxPooling2D
+from keras.layers import Input, Reshape
+from keras.models import Model
+from keras.optimizers import Adam
+from keras.optimizers import RMSprop
+from keras.utils.vis_utils import plot_model
+import keras
+
+
+def conv_model(n_input, n_output, n_feature, n_enc_units, n_dec_units, feat_units = 5):
+    root_word_input = Input(shape=(15, 28, 1), name="root_word_input")
+    feature_input = Input(shape=(n_feature,), name="word_feature_input")
+    feat_out = Dense(feat_units, activation="relu", name="feature_output")(feature_input)
+    
+    x = Conv2D(20, (5, 5), padding='same', activation='relu')(root_word_input)
+    x = MaxPooling2D(3, 3)(x)
+
+    flat_output = Flatten()(x)
+#     x = Concatenate()([x, feat_out])
+#     state_h = Dense(n_dec_units, activation='relu')(x)
+    x = Dense(n_dec_units - feat_units, activation='relu')(flat_output)
+    
+    state_h = Concatenate()([x, feat_out])
+    
+    decoder_inputs = Input(shape=(None, n_output), name="target_word_input")
+    decoder_gru = GRU(n_dec_units, return_sequences=True, return_state=True, name="decoder_gru")
+    decoder_outputs, _= decoder_gru(decoder_inputs, initial_state=state_h)
+    
+    decoder_dense = Dense(n_output, activation='softmax', name="train_output")
+    decoder_outputs = decoder_dense(decoder_outputs)
+    
+    model = Model([root_word_input, decoder_inputs, feature_input], decoder_outputs)
+    encoder_model = Model([root_word_input, feature_input], state_h)
+    
+    decoder_state_input_h = Input(shape=(n_dec_units,))
+    decoder_outputs, state_h= decoder_gru(decoder_inputs, initial_state=decoder_state_input_h)
+
+    decoder_outputs = decoder_dense(decoder_outputs)
+    decoder_model = Model([decoder_inputs, decoder_state_input_h], [decoder_outputs, state_h])
+
+    return model, encoder_model, decoder_model
+
+def predict(infenc, infdec, source, feat, n_steps, cardinality):
+    # encode
+    state = infenc.predict([source, feat])
+    # start of sequence input
+    start = [0.0 for _ in range(cardinality)]
+#     start[0] = 1
+    target_seq = np.array(start).reshape(1, 1, cardinality)
+    # collect predictions
+    output = list()
+    for t in range(n_steps):
+        # predict next char
+        yhat, h= infdec.predict([target_seq, state])
+        # store prediction
+        output.append(yhat[0,0,:])
+        # update state
+        state = h
+        # update target sequence
+        target_seq = yhat
+    return np.array(output)
+
