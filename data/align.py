dc5cadc2e858c9333d33e2d42222b230baf273d9
Leo Bitz <amanyamy@gmail.com>
2019-07-22 22:30:08 +0300

    LSTM in models

--- data/align.py
+++ data/align.py
@@ -1,35 +1,5 @@
-<<<<<<< HEAD
 import random
-lines = open("wol-m.txt").readlines()
-
-root_feat = {}
-for line in lines:
-    word, root, feat, i = line.split(' ')
-    suffix = word[len(root):]
-    key = root + " " + feat
-    if key not in root_feat:
-        root_feat[key] = []
-    root_feat[key].append(suffix)
-    root_feat[key] = sorted(root_feat[key])
-
-lines = []
-for key in root_feat:
-    root, feat = key.split(' ')
-    for i, suf in enumerate(root_feat[key]):
-        word = root + suf
-        line = "{0} {1} {2} {3}\n".format(word, root, feat, i)
-        lines.append(line)
-# lines = sorted(lines)
-random.shuffle(lines)
-
-file = open("wol-aligned.txt", mode="w")
-for line in lines:
-    file.write(line)
-
-file.close()
-=======
-import random
-lines = open("wol-m.txt").readlines()
+lines = open("manual.txt").readlines()
 
 root_feat = {}
 for line in lines:
@@ -59,9 +29,8 @@ for key in root_feat:
 
 random.shuffle(lines)
 
-file = open("wol-aligned.txt", mode="w")
+file = open("man-aligned.txt", mode="w")
 for line in lines:
     file.write(line)
 
 file.close()
->>>>>>> 5bdcf056fb1926f8d3757ef8b0a4f25e990cc48d
