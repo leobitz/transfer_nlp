b2bbf9f9cb31589d3f3986a0360f3bc0b4f832d0
Leo bitz <amanyamy@gmail.com>
2019-08-30 01:55:44 -0400

    batch file

--- cnn-hot.bat
+++ cnn-hot.bat
@@ -1,14 +1,14 @@
 @ECHO OFF
-for %%h in (512) do (
+for %%h in (256 512) do (
 	for %%c in (32) do (
-		for %%f in (8 16 32 ) do (
-			for %%d in (14 29 44 ) do (
+		for %%f in (16 32 64) do (
+			for %%d in (14 44 ) do (
 				for %%t in (1 2 3 4 5) do (
-					if exist "data\results\%%h-%%f-%%c-%%d-%%t.txt" (
+					if exist "data\results\lstm-atten-%%h-%%f-%%c-%%d-%%t.txt" (
 						echo "file exists"
 					) else (
-						echo "data\results\%%h-%%f-%%c-%%d-%%t.txt"
-						python atten-onehot.py --epochs 80 --batch_size 128 --hidden_size %%h --feat_embed_size %%h --char_embed_size %%c --data_size %%d  > data\results\%%h-%%f-%%c-%%d-%%t.txt
+						echo "data\results\lstm-atten-%%h-%%f-%%c-%%d-%%t.txt"
+						python lstm-atten-onehot.py --epochs 80 --batch_size 128 --hidden_size %%h --feat_embed_size %%h --char_embed_size %%c --data_size %%d  > data\results\lstm-atten-%%h-%%f-%%c-%%d-%%t.txt
 					)
 					
 				)
